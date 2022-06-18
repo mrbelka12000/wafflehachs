468d7f2d1a7aaa4360ac45959f51f9837c1a6e5c
mrbelka12000 <beka.teka11@gmail.com>
2022-06-19 02:33:20 +0600

    check port

--- internal/server/Build.go
+++ internal/server/Build.go
@@ -9,10 +9,14 @@ import (
 )
 
 func NewServer(h *handler.Handler) *http.Server {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	return &http.Server{
 		WriteTimeout: time.Duration(25 * time.Second),
 		ReadTimeout:  time.Duration(25 * time.Second),
 		Handler:      routes.SetUpMux(h),
-		Addr:         ":" + os.Getenv("Port"),
+		Addr:         ":" + port,
 	}
 }
