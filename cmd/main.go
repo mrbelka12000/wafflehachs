66dfd00fdf3a781d69bc13e388ba077078e1cf60
Beknur.Karshyga <beka.teka11@gmail.com>
2022-06-20 02:49:15 +0600

    gr sh added

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"os"
-	"wafflehacks/internal/app"
-	"wafflehacks/internal/server"
-
+	"context"
 	"go.uber.org/zap"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"wafflehacks/database"
+	"wafflehacks/entities/storage"
+	"wafflehacks/internal/app"
 )
 
 func main() {
@@ -15,11 +20,40 @@ func main() {
 	}
 	log := logger.Sugar()
 	defer log.Sync()
-	handler, err := app.Initialize(log)
+
+	db, err := database.GetConnection()
 	if err != nil {
-		log.Fatal("Error while conecting to postgres: ", err)
+		log.Fatal(err)
 	}
-	srv := server.NewServer(handler)
+
+	srv := app.Initialize(db, log)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 	log.Info("Server started on port: " + os.Getenv("PORT"))
-	log.Fatal(srv.ListenAndServe())
+
+	<-done
+	log.Info("Server Stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling
+		if err = db.Close(); err != nil {
+			log.Debug(err.Error())
+		}
+		if err = os.Remove(storage.GoogleConfigFileName); err != nil {
+			log.Debug(err.Error())
+		}
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Info("Server Exited Properly")
 }
--- internal/app/Initialize.go
+++ internal/app/Initialize.go
@@ -1,29 +1,26 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-	"wafflehacks/database"
 	"wafflehacks/entities/storage"
 	"wafflehacks/internal/handler"
 	"wafflehacks/internal/repository"
+	"wafflehacks/internal/server"
 	"wafflehacks/internal/service"
 	"wafflehacks/tools"
 
 	"go.uber.org/zap"
 )
 
-func Initialize(log *zap.SugaredLogger) (*handler.Handler, error) {
-	db, err := database.GetConnection()
-	if err != nil {
-		return nil, err
-	}
-
-	database.UpTables(db, log)
+func Initialize(db *sql.DB, log *zap.SugaredLogger) *http.Server {
 	repo := repository.NewRepo(db, log)
-	srv := service.NewService(repo, log)
-	return handler.NewHandler(srv, log), nil
+	serv := service.NewService(repo, log)
+	handler := handler.NewHandler(serv, log)
+	return server.NewServer(handler)
 }
 
 func init() {
