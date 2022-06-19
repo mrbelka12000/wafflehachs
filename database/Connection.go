14514d97d9c9b39296f72e6c87296006a07c3797
mrbelka12000 <beka.teka11@gmail.com>
2022-06-19 22:30:35 +0600

    ssl disable

--- database/Connection.go
+++ database/Connection.go
@@ -23,7 +23,7 @@ func getConnectionString() string {
 		return connStrForHeroku
 	}
 
-	connStrForDocker := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=require",
+	connStrForDocker := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
 		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"),
 		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_DB"))
---
+++ testserver/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"time"
+	"wafflehacks/tools"
+
+	"cloud.google.com/go/storage"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	projectID  = "gcp-storage-353811" // FILL IN WITH YOURS
+	bucketName = "wafflehacksbucket"  // FILL IN WITH YOURS
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
+var uploader *ClientUploader
+
+func init() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "gcp-storage-353811-065f9a465bfd.json") // FILL IN WITH YOUR FILE PATH
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	uploader = &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "test-files/",
+	}
+}
+
+func main() {
+	// uploader.UploadFile("notes_test/abc.txt")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.Header("Content/type", "text/html")
+		c.Writer.Write([]byte(`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<link rel="stylesheet" type="text/css" href="assets/css/createpost.css">
+			<link rel="shortcut icon" href="#">
+			<title>Create Post</title>
+		</head>
+		<body>
+			<div class="title">Create post</div>
+			<form method="POST"enctype=multipart/form-data action="/upload" >
+				<div class="container">
+			 
+					<input type="file" accept=".jpg, .jpeg, .png , .gif"name="file_input"/>
+				 
+					</div>
+					<br> 
+			 
+					<div class="btn_container" style="background-color:#f1f1f1">
+						<button class="btn" type="submit">Create</button>
+						<input class="btn" type="button" value="Home" onClick='location.href="http://localhost:8080/"'>
+					</div>
+				</div>
+			</form>
+		</body>
+		</html>
+		`))
+	})
+	r.POST("/upload", func(c *gin.Context) {
+		f, err := c.FormFile("file_input")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		blobFile, err := f.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		err = uploader.UploadFile(blobFile, tools.GetRandomString())
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+// UploadFile uploads an object
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	fmt.Println(wc.Name)
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
